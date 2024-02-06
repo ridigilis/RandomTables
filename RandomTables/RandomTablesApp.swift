7e3b02206f41fe570935254e6de30372326c634b
ridigilis <ridigilis@ridigilis.dev>
2024-02-05 18:02:25 -0600

    Initial Commit

---
+++ RandomTables/RandomTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTablesApp.swift
+//  RandomTables
+//
+//  Created by Ricky David Groner II on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
